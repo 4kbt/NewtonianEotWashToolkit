db513920cfcbdf1d9b1461fc9f80af6b179473da
Charlie Hagedorn <cah49@uw.edu>
2015-11-18 15:31:34 -0800

    Faster annular sheet generation

--- shapes/genPointMassAnnlSheet.m
+++ shapes/genPointMassAnnlSheet.m
@@ -10,7 +10,9 @@ function array= genPointMassAnnlSheet(mass, iRadius, oRadius, thickness, ...
 	
 	pointMass = density * xgrid * ygrid * zgrid;
 
-	array = [];
+	array = zeros(nxpoints*nypoints*nzpoints, 4);
+
+	loopcounter = 1;
 
 	for i = 1:nxpoints
 		for j = 1:nypoints
@@ -23,14 +25,36 @@ function array= genPointMassAnnlSheet(mass, iRadius, oRadius, thickness, ...
 				pRadius = sqrt( newMass(3)^2 + newMass(4)^2 );
 
 				if(pRadius <= oRadius && pRadius >= iRadius)
-					array = [ array ; newMass ];
+					array(loopcounter,:) = newMass;
+					loopcounter = loopcounter + 1;
 				end
 			end
 		end
 	end
+
+	%decrement loopcounter to undo final increment;
+	loopcounter = loopcounter - 1;
+
+	%trim array to appropriate size
+	array = array(1:loopcounter,:);
 
 	%Correcting any mass discrepancy
 	MassDiscrepancyRatio = sum( array(:,1) ) / mass;
 	array(:,1) = array(:,1) / MassDiscrepancyRatio;
 	MassDiscrepancyRatio = sum( array(:,1) ) / mass;
 end
+
+%!test
+%!
+%! M = genPointMassAnnlSheet( 1, 0, 1, 1, 2, 2);
+%!
+%! %Are there the expected eight points?
+%! assert( rows(M) == 8 );
+%! 
+%! %Are all the points the same distance from the origin?
+%! r2 = M(1,2)^2 + M(1,3)^2 + M(1,4)^2;
+%! assert(  ones(rows(M),1) * r2 == M(:,2).^2 + M(:,3).^2 + M(:,4).^2);
+%! 
+%! %Is mass properly normalized?
+%! assert( sum(M(:,1)) == 1);
+%! assert( sum(abs( diff( M(:,1) ) ) )  < rows(M) * eps);
