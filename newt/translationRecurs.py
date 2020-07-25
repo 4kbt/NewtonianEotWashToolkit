536b396e2a286a29f1d0b205463bc549d3b2df36
JGLee6 <j.greendeer.lee@gmail.com>
2020-07-25 13:41:50 -0700

    Translation matrices for modified helmholtz (MH) and R-R Laplace. Can't test MH version, L works on prelim test

--- newt/translationRecurs.py
+++ newt/translationRecurs.py
@@ -6,9 +6,10 @@ Created on Thu Apr 23 08:16:19 2020
 """
 import numpy as np
 import scipy.special as sp
+import scipy.linalg as sla
 
 
-def transl_yuk_z_SR_recurs(L, k, dr):
+def transl_yuk_z_SR_recurs(LMax, dr, k):
     """
     Translate coaxially from regular to singular, Gumerov and Duraiswami.
 
@@ -24,20 +25,54 @@ def transl_yuk_z_SR_recurs(L, k, dr):
 
     https://arxiv.org/pdf/1403.7698v1.pdf
     """
-    ls = np.arange(L)
-    sr = np.zeros([L+1, L+1], dtype=complex)
-    sr[:, 0] = (-1)**(ls)*np.sqrt(2*ls+1)*sp.spherical_kn(ls, k*dr)
-    for l in np.arange(L):
-        for m in range(l):
-            blmm = bnm(l, m-1)
-            blmp = bnm(l+1, m)
-            bmm = bnm(m+1, -m-1)
-            print(blmm, blmp, bmm)
-            sr[l, m+1] = (sr[l-1, m]*blmm - sr[l+1, m]*blmp)/bmm
-    return sr
+    # Create (E|F)_{l,m}^{m}
+    eflm = np.zeros([2*LMax+1, LMax+1])
+    # Start (E|F)_{l, 0}^{0}
+    ls = np.arange(2*LMax+1)
+    eflm[:, 0] = (-1)**ls*np.sqrt(2*ls+1)*sp.spherical_kn(ls, k*dr)
+    # now recursion 4.86 for (E|F)_{l, m}^{m}
+    for m in range(LMax):
+        for l in range(2*LMax-m):
+            # (n=m)
+            blmm = get_bnm(l, -m-1)
+            blmp = get_bnm(l+1, m)
+            bmm = get_bnm(m+1, -m-1)
+            eflm[l, m+1] = (eflm[l-1, m]*blmm - eflm[l+1, m]*blmp)/bmm
+
+    efms = []
+    # Now create (E|F)^m matrices (p-|m|)x(p-|m|)
+    # Start with (2*p-|m|)x(p-|m|) and truncate
+    for m in range(LMax+1):
+        efm = np.zeros([2*LMax+1-2*m, LMax-m+1])
+        efm[:, 0] = eflm[m:2*LMax+1-m, m]
+        for n in range(LMax-m):
+            for l in range(n+1, 2*(LMax-m)-n):
+                anm = get_anm(n+m, m)
+                alm = get_anm(l+m, m)
+                almm = get_anm(l-1+m, m)
+                anmm = get_anm(n-1+m, m)
+                print(l, n+1, l+m, anm, anmm, alm, almm)
+                if anmm != 0:
+                    print(l, n, m)
+                    efm[l, n+1] = (almm*efm[l-1, n] - alm*efm[l+1, n]
+                                   + anmm*efm[l, n-1])/anm
+                else:
+                    efm[l, n+1] = (alm*efm[l+1, n] - almm*efm[l-1, n])/anm
+        efms.append(efm)
+
+    for m in range(LMax+1):
+        efms[m] = efms[m][:LMax-m+1, :LMax-m+1]
+        lm = len(efms[m])
+        # Start from m since (E|F)^m matrix starts at (m,m) corner
+        nls = (np.arange(m, m+lm))
+        enl = np.transpose(efms[m])*np.outer((-1)**(nls), (-1)**(nls))
+        efms[m] += enl
+        efms[m] -= np.diag(np.diag(enl))
+
+    return eflm, efms
 
 
-def transl_yuk_z_RR_recurs(L, k, dr):
+def transl_yuk_z_RR_recurs(LMax, dr, k):
     """
     Translate coaxially from regular to regular, Gumerov and Duraiswami.
 
@@ -53,20 +88,54 @@ def transl_yuk_z_RR_recurs(L, k, dr):
 
     https://arxiv.org/pdf/1403.7698v1.pdf
     """
-    ls = np.arange(L+1)
-    rr = np.zeros([L+1, L+1], dtype=complex)
-    rr[:, 0] = (-1)**(ls)*np.sqrt(2*ls+1)*sp.spherical_in(ls, k*dr)
-    for l in np.arange(L):
-        for m in range(l):
-            blmm = bnm(l, m-1)
-            blmp = bnm(l+1, m)
-            bmm = bnm(m+1, -m-1)
-            print(blmm, blmp, bmm)
-            rr[l, m+1] = (rr[l-1, m]*blmm - rr[l+1, m]*blmp)/bmm
-    return rr
+    # Create (E|F)_{l,m}^{m}
+    eflm = np.zeros([2*LMax+1, LMax+1])
+    # Start (E|F)_{l, 0}^{0}
+    ls = np.arange(2*LMax+1)
+    eflm[:, 0] = (-1)**ls*np.sqrt(2*ls+1)*sp.spherical_in(ls, k*dr)
+    # now recursion 4.86 for (E|F)_{l, m}^{m}
+    for m in range(LMax):
+        for l in range(2*LMax-m):
+            # (n=m)
+            blmm = get_bnm(l, -m-1)
+            blmp = get_bnm(l+1, m)
+            bmm = get_bnm(m+1, -m-1)
+            eflm[l, m+1] = (eflm[l-1, m]*blmm - eflm[l+1, m]*blmp)/bmm
+
+    efms = []
+    # Now create (E|F)^m matrices (p-|m|)x(p-|m|)
+    # Start with (2*p-|m|)x(p-|m|) and truncate
+    for m in range(LMax+1):
+        efm = np.zeros([2*LMax+1-2*m, LMax-m+1])
+        efm[:, 0] = eflm[m:2*LMax+1-m, m]
+        for n in range(LMax-m):
+            for l in range(n+1, 2*(LMax-m)-n):
+                anm = get_anm(n+m, m)
+                alm = get_anm(l+m, m)
+                almm = get_anm(l-1+m, m)
+                anmm = get_anm(n-1+m, m)
+                print(l, n+1, l+m, anm, anmm, alm, almm)
+                if anmm != 0:
+                    print(l, n, m)
+                    efm[l, n+1] = (almm*efm[l-1, n] - alm*efm[l+1, n]
+                                   + anmm*efm[l, n-1])/anm
+                else:
+                    efm[l, n+1] = (alm*efm[l+1, n] - almm*efm[l-1, n])/anm
+        efms.append(efm)
+
+    for m in range(LMax+1):
+        efms[m] = efms[m][:LMax-m+1, :LMax-m+1]
+        lm = len(efms[m])
+        # Start from m since (E|F)^m matrix starts at (m,m) corner
+        nls = (np.arange(m, m+lm))
+        enl = np.transpose(efms[m])*np.outer((-1)**(nls), (-1)**(nls))
+        efms[m] += enl
+        efms[m] -= np.diag(np.diag(enl))
+
+    return eflm, efms
 
 
-def transl_newt_z_SR_recurs(L, dr):
+def transl_newt_z_SR_recurs(LMax, dr):
     """
     Translate coaxially from regular to singular, Gumerov and Duraiswami.
 
@@ -82,20 +151,54 @@ def transl_newt_z_SR_recurs(L, dr):
 
     https://arxiv.org/pdf/1403.7698v1.pdf
     """
-    ls = np.arange(L)
-    sr = np.zeros([L+1, L+1], dtype=complex)
-    sr[:, 0] = (-1)**(ls)*np.sqrt(2*ls+1)/dr**(ls+1)
-    for l in np.arange(L):
-        for m in range(l):
-            blmm = bnm(l, m-1)
-            blmp = bnm(l+1, m)
-            bmm = bnm(m+1, -m-1)
-            print(blmm, blmp, bmm)
-            sr[l, m+1] = (sr[l-1, m]*blmm - sr[l+1, m]*blmp)/bmm
-    return sr
+    # Create (E|F)_{l,m}^{m}
+    eflm = np.zeros([2*LMax+1, LMax+1])
+    # Start (E|F)_{l, 0}^{0}
+    ls = np.arange(2*LMax+1)
+    eflm[:, 0] = (-1)**ls*np.sqrt(2*ls+1)*sp.factorial2(2*ls-1)/dr**(ls+1)
+    # now recursion 4.86 for (E|F)_{l, m}^{m}
+    for m in range(LMax):
+        for l in range(2*LMax-m):
+            # (n=m)
+            blmm = get_bnm(l, -m-1)
+            blmp = get_bnm(l+1, m)
+            bmm = get_bnm(m+1, -m-1)
+            eflm[l, m+1] = (eflm[l-1, m]*blmm - eflm[l+1, m]*blmp)/bmm
+
+    efms = []
+    # Now create (E|F)^m matrices (p-|m|)x(p-|m|)
+    # Start with (2*p-|m|)x(p-|m|) and truncate
+    for m in range(LMax+1):
+        efm = np.zeros([2*LMax+1-2*m, LMax-m+1])
+        efm[:, 0] = eflm[m:2*LMax+1-m, m]
+        for n in range(LMax-m):
+            for l in range(n+1, 2*(LMax-m)-n):
+                anm = get_anm(n+m, m)
+                alm = get_anm(l+m, m)
+                almm = get_anm(l-1+m, m)
+                anmm = get_anm(n-1+m, m)
+                print(l, n+1, l+m, anm, anmm, alm, almm)
+                if anmm != 0:
+                    print(l, n, m)
+                    efm[l, n+1] = (almm*efm[l-1, n] - alm*efm[l+1, n]
+                                   + anmm*efm[l, n-1])/anm
+                else:
+                    efm[l, n+1] = (alm*efm[l+1, n] - almm*efm[l-1, n])/anm
+        efms.append(efm)
+
+    for m in range(LMax+1):
+        efms[m] = efms[m][:LMax-m+1, :LMax-m+1]
+        lm = len(efms[m])
+        # Start from m since (E|F)^m matrix starts at (m,m) corner
+        nls = (np.arange(m, m+lm))
+        enl = np.transpose(efms[m])*np.outer((-1)**(nls), (-1)**(nls))
+        efms[m] += enl
+        efms[m] -= np.diag(np.diag(enl))
+
+    return eflm, efms
 
 
-def transl_newt_z_RR_recurs(L, k, dr):
+def transl_newt_z_RR_recurs(LMax, dr):
     """
     Translate coaxially from regular to regular, Gumerov and Duraiswami.
 
@@ -111,21 +214,123 @@ def transl_newt_z_RR_recurs(L, k, dr):
 
     https://arxiv.org/pdf/1403.7698v1.pdf
     """
-    ls = np.arange(L+1)
-    rr = np.zeros([L+1, L+1], dtype=complex)
-    rr[:, 0] = np.sqrt(2*ls+1)*(-dr)**(ls)
-    for l in np.arange(L):
-        for m in range(l):
-            blmm = bnm(l, m-1)
-            blmp = bnm(l+1, m)
-            bmm = bnm(m+1, -m-1)
-            print(blmm, blmp, bmm)
-            rr[l, m+1] = (rr[l-1, m]*blmm - rr[l+1, m]*blmp)/bmm
-    return rr
+    # Create (E|F)_{l,m}^{m}
+    eflm = np.zeros([2*LMax+1, LMax+1])
+    # Start (E|F)_{l, 0}^{0}
+    ls = np.arange(2*LMax+1)
+    eflm[:, 0] = (-1)**ls*np.sqrt(2*ls+1)*dr**ls/sp.factorial2(2*ls+1)
+    # now recursion 4.86 for (E|F)_{l, m}^{m}
+    for m in range(LMax):
+        for l in range(2*LMax-m):
+            # (n=m)
+            blmm = get_bnm(l, -m-1)
+            blmp = get_bnm(l+1, m)
+            bmm = get_bnm(m+1, -m-1)
+            eflm[l, m+1] = (eflm[l-1, m]*blmm - eflm[l+1, m]*blmp)/bmm
+
+    efms = []
+    # Now create (E|F)^m matrices (p-|m|)x(p-|m|)
+    # Start with (2*p-|m|)x(p-|m|) and truncate
+    for m in range(LMax+1):
+        efm = np.zeros([2*LMax+1-2*m, LMax-m+1])
+        efm[:, 0] = eflm[m:2*LMax+1-m, m]
+        for n in range(LMax-m):
+            for l in range(n+1, 2*(LMax-m)-n):
+                anm = get_anm(n+m, m)
+                alm = get_anm(l+m, m)
+                almm = get_anm(l-1+m, m)
+                anmm = get_anm(n-1+m, m)
+                print(l, n+1, l+m, anm, anmm, alm, almm)
+                if anmm != 0:
+                    print(l, n, m)
+                    efm[l, n+1] = (almm*efm[l-1, n] - alm*efm[l+1, n]
+                                   + anmm*efm[l, n-1])/anm
+                else:
+                    efm[l, n+1] = (alm*efm[l+1, n] - almm*efm[l-1, n])/anm
+        efms.append(efm)
+
+    for m in range(LMax+1):
+        efms[m] = efms[m][:LMax-m+1, :LMax-m+1]
+        lm = len(efms[m])
+        # Start from m since (E|F)^m matrix starts at (m,m) corner
+        nls = (np.arange(m, m+lm))
+        enl = np.transpose(efms[m])*np.outer((-1)**(nls), (-1)**(nls))
+        efms[m] += enl
+        efms[m] -= np.diag(np.diag(enl))
+
+    return eflm, efms
 
 
-def bnm(n, m):
-    bval = np.sqrt((n-m-1)*(n-m)/((2*n-1)*(2*n+1)))*np.sign(n)
+def transl_newt_z_RR_recurs2(LMax, dr):
+    """
+    Translate coaxially from regular to regular, Gumerov and Duraiswami. In a
+    slight contradiction to the paper, our choice of normalization (alpha_n^m)
+    does not require all three normalization factors in the translation matrix,
+    see eqn 19.
+
+    Inputs
+    ------
+    l : int
+        Order of multipole expansion to output rotation matrix coefficient H
+
+    Reference
+    ---------
+    "Comparison of the efficiency of translation operators used in the fast
+    multipole method for the 3D Laplace equation"
+
+    http://legacydirs.umiacs.umd.edu/~gumerov/PDFs/cs-tr-4701.pdf
+    """
+    # Descending array of lp from LMax through 0
+    lp = np.arange(LMax+1)
+    rvals = (-dr)**lp/sp.factorial(lp)
+    cols = [rvals[0], *np.zeros(LMax)]
+    rrms = []
+    fac = alphanm(lp, 0)
+    fac2 = np.outer(fac, 1/fac)
+    rrms.append((sla.toeplitz(cols, rvals)*fac2).T)
+    # Each m has a matrix that is (LMax-|m|)x(LMax-|m|) in size
+    for m in range(1, LMax+1):
+        fac = alphanm(np.arange(m, LMax+1), m)
+        fac2 = np.outer(fac, 1/fac)
+        rrm = sla.toeplitz(cols[:-m], rvals[:-m])*fac2
+        rrms.append(rrm.T)
+    return rrms
+
+
+def alphanm(n, m):
+    anm = (-1)**n*1j**(-np.abs(m))*np.sqrt(4*np.pi/((2*n+1)*sp.factorial(n-m)*sp.factorial(n+m)))
+    return anm
+
+
+def apply_trans_mat(qlm, efms):
+    L = len(qlm)-1
+    print(L)
+    qNew = np.zeros([L+1, 2*L+1], dtype='complex')
+    qNew[:, L] = np.dot(efms[0], qlm[:, L])
+    for m in range(1, L+1):
+        qNew[m:, L+m] = np.dot(efms[m], qlm[m:, L+m])
+        qNew[m:, L-m] = np.dot(efms[m], qlm[m:, L-m])
+    return qNew
+
+
+def get_bnm(n, m):
+    """
+    B-recursion function for translation matrices.
+    """
     if np.abs(m) > n:
         bval = 0
+    else:
+        bval = np.sqrt((n-m-1)*(n-m)/((2*n-1)*(2*n+1)))*np.sign(n)
     return bval
+
+
+def get_anm(n, m):
+    """
+    A-recursion function for translation matrices.
+    """
+    am = abs(m)
+    if am > n:
+        aval = 0
+    else:
+        aval = np.sqrt((n+am+1)*(n-am+1)/((2*n+1)*(2*n+3)))
+    return aval
